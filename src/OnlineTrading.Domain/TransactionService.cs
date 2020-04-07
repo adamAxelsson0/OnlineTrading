9b78046512fd06452d7395f4c068d820f57158f6
Jason Whetton <jason@digital-arkitekt.se>
2020-04-07 21:56:07 +0200

    added hint comment

--- src/OnlineTrading.Domain/TransactionService.cs
+++ src/OnlineTrading.Domain/TransactionService.cs
@@ -19,8 +19,8 @@ namespace OnlineTrading.Domain
         {
             var response = this.shareTradeService.GetQuoteForTicker(ticker);
 
-            if (response.QuoteId == null) throw new InvalidOperationException("Failed to retrieve a quote.");
-            
+            if (response.QuoteId == null) throw new InvalidOperationException("Failed to retrieve a quote (you need a stub!!).");
+
             repository.AddQuote(new Quote {
                 Id = id,
                 Quantity = quantity,
